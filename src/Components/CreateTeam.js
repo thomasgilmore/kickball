283f2ccc5a2887da715426893bb60e4912a8e3b1
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-10-13 19:04:14 -0400

    CreateTeam components form

---
+++ src/Components/CreateTeam.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function CreateTeam() {
+    return (
+        <div>
+            <form>
+                <label>Team Name:
+                <input type="text" name="Team Name" />
+                </label>
+                <label>Team Color:
+                <input type="text" name="Team Color"></input>
+                </label>
+            </form>
+        </div>
+    )
+}
