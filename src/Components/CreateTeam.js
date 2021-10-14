4b14c7bda853f822e7ea9cb4591ea457fa4dba9b
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-10-14 18:49:09 -0400

    CreateTeam Component

--- src/Components/CreateTeam.js
+++ src/Components/CreateTeam.js
@@ -1,17 +1,24 @@
 import React from 'react'
 
 export default function CreateTeam() {
-    return (
-        <div>
-            <form>
-                <label>Team Name:
-                <input type="text" name="Team Name" />
-                </label>
-                <label>Team Color:
-                <input type="text" name="Team Color"></input>
-                </label>
-                <button>Create Team</button>
-            </form>
-        </div>
-    )
+  const handleSubmit = e => {
+    e.preventDefault()
+    // console.log(e.target[0].value);
+    // console.log(e.target[1].value);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Team Name:
+          <input type="text" name="Team Name" />
+        </label>
+        <label>
+          Team Color:
+          <input type="text" name="Team Color"></input>
+        </label>
+        <button>Create Team</button>
+      </form>
+    </div>
+  )
 }
