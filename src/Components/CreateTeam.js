bbdc305280a4d734bb28f72cad40d404cb48d628
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-10-26 11:29:09 -0400

    changing to following airbnb style guide

--- src/Components/CreateTeam.js
+++ src/Components/CreateTeam.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './createteam.css';
 
 export default function CreateTeam() {
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     // console.log(e.target[0].value);
     // console.log(e.target[1].value);
-  }
+  };
   return (
     <div>
       <form className="createTeamForm" onSubmit={handleSubmit}>
@@ -15,10 +16,10 @@ export default function CreateTeam() {
         </label>
         <label>
           Team Color:
-          <input type="text" name="Team Color"></input>
+          <input type="text" name="Team Color" />
         </label>
-        <button className="createTeamButton">Create Team</button>
+        <button type="button" className="createTeamButton">Create Team</button>
       </form>
     </div>
-  )
+  );
 }
