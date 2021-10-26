2fcddcd3da02a0e60cdd867edc657973c69e4bf0
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-10-26 11:45:56 -0400

    updated App.js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreateTeam from './Components/CreateTeam';
+
+function App() {
+  return (
+    <div className="App">
+      <CreateTeam />
+    </div>
+  );
+}
+
+export default App;
