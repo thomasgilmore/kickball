3e72f0f282d63fc7e90b48a1dae76f7604016b5c
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-11-02 14:35:21 -0400

    added new components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import CreateTeam from './Components/CreateTeam';
+import PlayerInput from './Components/PlayerInput';
+import PlayerInput2 from './Components/PlayerInput2';
 
 function App() {
   return (
     <div className="App">
       <CreateTeam />
+      <PlayerInput />
+      <PlayerInput2 />
     </div>
   );
 }
