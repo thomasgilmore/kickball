9172e73be5420e0dd9e5b068ccfd80265943673c
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-11-03 19:24:21 -0400

    removed PlayerInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import PlayerInput from './Components/PlayerInput';
 import PlayerInput2 from './Components/PlayerInput2';
 
 function App() {
   return (
     <div className="App">
-      <PlayerInput />
       <PlayerInput2 />
     </div>
   );
