0b8d5b7d55ac1cbfbe6403e698cc346c6d90da2c
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-11-11 21:23:06 -0500

    added useState to update form

--- src/Components/PlayerInput2.jsx
+++ src/Components/PlayerInput2.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './playerInput.css';
 
 export default function PlayerInput2() {
+  const [yesButton, setYesButton] = useState('100%');
+  const [noButton, setNoButton] = useState('100%');
+  const [whichButtonSelected, setWhichButtonSelected] = useState('None');
+  const [messageAfterSubmit, setMessageAfterSubmit] = useState('');
+  const [viewMessage, setViewMessage] = useState('No');
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  // let yesButton = '100%';
+  // let noButton = '100%';
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (whichButtonSelected !== 'None') {
+      setViewMessage('Yes');
+      setButtonDisabled(true);
+    }
+    // console.log(whichButtonSelected);
     // console.log(e.target[0].value);
     // console.log(e.target[1].value);
   };
 
   const handleYesClick = (e) => {
     e.preventDefault();
+    setYesButton('100%');
+    setNoButton('50%');
+    setWhichButtonSelected('Yes');
+    setMessageAfterSubmit('Well see you at the game!');
   };
 
   const handleNoClick = (e) => {
     e.preventDefault();
+    setYesButton('50%');
+    setNoButton('100%');
+    setWhichButtonSelected('No');
+    setMessageAfterSubmit("Sorry, we'll miss you at the game");
   };
 
   return (
@@ -26,10 +48,11 @@ export default function PlayerInput2() {
         <h1>
           Going to Game:
         </h1>
-        <input type="button" className="playerSubmitButton" value="Yes" onClick={handleYesClick} />
-        <input type="button" className="playerSubmitButton" value="No" onClick={handleNoClick} />
-        <button type="button" className="playerSubmitButton">Submit</button>
+        <input type="button" className="playerChoiceButtons Yes" disabled={buttonDisabled} style={{ opacity: yesButton }} value="Yes" onClick={handleYesClick} />
+        <input type="button" className="playerChoiceButtons No" disabled={buttonDisabled} style={{ opacity: noButton }} value="No" onClick={handleNoClick} />
+        <button type="button" className="playerSubmitButton" onClick={handleSubmit}>Submit</button>
       </form>
+      {viewMessage === 'Yes' ? <h3>{messageAfterSubmit}</h3> : <div />}
     </div>
   );
 }
