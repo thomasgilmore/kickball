f1eba6f2db996e8b5b81275a1237aa77bd652673
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-06-04 17:41:39 -0400

    working on PlayerInout3

--- src/Components/PlayerInput3.js
+++ src/Components/PlayerInput3.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Button, ButtonGroup, Input, Typography } from '@mui/material';
+import { Button, ToggleButton, ToggleButtonGroup, Input, Typography } from '@mui/material';
 
 export default function PlayerInput3() {
+  const [alignment, setAlignment] = React.useState('');
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
   return (
-    <div>
-      <form className="">
-        <label htmlFor="playerName">
-          Name:
-          <Input name="Player Name" />
-        </label>
-        <Typography variant='h3' align='center'>
-          Going to Game:
+    <div className='flex'>
+      <form className="mx-auto">
+        <div className='flex no-wrap'>
+          <label className='mt-7 text-xl'>
+            Name:
+          </label>
+          <Input name="Player Name" className='my-6' fullWidth={true} />
+        </div>
+        <Typography variant='h4' align='center'>
+          Going to the Game?
         </Typography>
-        <ButtonGroup>
-          <Button>Yes</Button>
-          <Button color="secondary">No</Button>
-        </ButtonGroup>
-        <Button variant="contained">Submit</Button>
+        <ToggleButtonGroup 
+          className='my-6' 
+          value={alignment}
+          exclusive
+          fullWidth={true}
+          onChange={handleChange}
+        >
+          <ToggleButton color='success' size="large" value="yes">Yes</ToggleButton>
+          <ToggleButton color="error" size="large" value="no">No</ToggleButton>
+        </ToggleButtonGroup>
+        <div className='flex flex-col'>
+          <Button variant="contained" size="large">Submit</Button>
+        </div>
       </form>
     </div>
   )
