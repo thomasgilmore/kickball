9bb618b99f567762ce3e285a36b7afe77905e9c9
Thomas Gilmore <thomasgilmore95@gmail.com>
2022-06-15 11:57:54 -0400

    added configureStore to index.js

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './Store/store';
+import { configureStore } from '@reduxjs/toolkit';
+import kickballApp from './Store/store';
 import App from './App';
+
+const store = configureStore({ reducer: kickballApp });
 
 ReactDOM.render(
   <React.StrictMode>
